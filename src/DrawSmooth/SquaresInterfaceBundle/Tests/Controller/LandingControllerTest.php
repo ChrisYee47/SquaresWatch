355ebbe77c1f68ba6013d3d78ed700ca930c9330
Chris Yee <cyee@online-buddies.com>
2013-03-31 20:56:55 -0400

    Add missing ST2 files

---
+++ src/DrawSmooth/SquaresInterfaceBundle/Tests/Controller/LandingControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DrawSmooth\SquaresInterfaceBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LandingControllerTest extends WebTestCase
+{
+}
